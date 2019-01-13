51fa6a44bc4344e7833740fb94b28fafb9f87885
hbdMeghana <hbdMeghana@gmail.com>
2019-01-13 19:46:15 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ArrayType } from '@angular/compiler/src/output/output_ast';
 
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'happyBirthdayMeghana';
+  title = 'Many Many Happy Returns of the day';
+  ngOnInit(){
+    $("#slideshow > div:gt(0)").hide();
+    
+    setInterval(function() { 
+      $('#slideshow > div:first')
+        .fadeOut(1500)
+        .next()
+        .fadeIn(1500)
+        .end()
+        .appendTo('#slideshow');
+    },  3000);
+    $('button').click(function(){
+      alert('Hello');
+    });
+    
+
+
+
+
+    
+
+   
+    
+   
+  }
 }
